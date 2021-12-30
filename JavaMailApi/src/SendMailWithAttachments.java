abd592745b3b3df656a80ab6481d8bf9971a8f58
Saikrishna-8 <88083193+Saikrishna-8@users.noreply.github.com>
2021-12-30 11:04:15 +0530

    first commit

--- JavaMailApi/src/SendMailWithAttachments.java
+++ JavaMailApi/src/SendMailWithAttachments.java
@@ -20,7 +20,7 @@ public class SendMailWithAttachments {
 	public static void main(String[] args) throws AddressException {
 
 		String host = "smtp.gmail.com";
-		final String user = "221710307008@gitam.in";// change accordingly
+		final String user = "221710307008@gitam.in";
 		final String password = "Fassak@10";// change accordingly
 		String to[] = { "saibadineedi@gmail.com"/* ,"221710307006@gitam.in","221710307049@gitam.in" */ };// change
 																											// accordingly
@@ -53,32 +53,23 @@ public class SendMailWithAttachments {
 			message.setText("This is simple program of sending email using JavaMail API");
 
 			Multipart multipart = new MimeMultipart();
-
 			MimeBodyPart attachmentPart = new MimeBodyPart();
-
 			MimeBodyPart textPart = new MimeBodyPart();
-
 			try {
-
 				File f = new File("C:\\Users\\sai\\eclipse-workspace\\JavaMailApi\\src\\brahmi.jpg");
-
 				attachmentPart.attachFile(f);
 				textPart.setText("Brahmi");
 				multipart.addBodyPart(textPart);
 				multipart.addBodyPart(attachmentPart);
-
 			} catch (IOException e) {
-
 				e.printStackTrace();
-
 			}
-
 			message.setContent(multipart);
 
-			message.setContent("<h2>HTML Image</h2>\r\n"
+			/*message.setContent("<h2>HTML Image</h2>\r\n"
 					+ "<img src=\"C:\\Users\\sai\\eclipse-workspace\\JavaMailApi\\src\\brahmi.jpg\" alt=\"Trulli\" width=\"500\" height=\"333\">",
 					"text/html");
-
+					*/
 			// send the message
 			Transport.send(message);
 
